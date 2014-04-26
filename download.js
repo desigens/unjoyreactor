9b440d641eee2bd6a017ef3b37b84f6bf63feec6
Denis Denisov <desigens@gmail.com>
2014-04-26 19:30:03 +0400

    show saved file after post form

--- download.js
+++ download.js
@@ -10,14 +10,15 @@ Download.prototype.url = function (url, callback) {
 	var md5sum = crypto.createHash('md5');
 	md5sum.update(url);
 	var hex = md5sum.digest('hex');
+	var path = 'images/' + hex;
 
-	if (!fs.existsSync(hex)) {
+	if (!fs.existsSync(path)) {
 		request.get({
 			url: url,
 			encoding: 'binary'
 		}, function (error, response, body) {
 		    if (!error && response.statusCode == 200) {
-		        fs.writeFile(hex, body, 'binary', function(err){
+		        fs.writeFile(path, body, 'binary', function(err){
 		            if (err) throw err
 		            console.log('File saved.')
 
@@ -27,6 +28,7 @@ Download.prototype.url = function (url, callback) {
 		    }
 		});
 	} else {
+		console.log('File was on disk')
 		if (callback) callback(hex);
 	}
 }
--- server.js
+++ server.js
@@ -8,14 +8,29 @@ var app = express();
 
 app.use(bodyParser());
 
+app.get('/images/:hash', function(req, res){  	
+	res.type('jpg');
+  	res.sendfile('images/' + req.params.hash);
+});
+
+app.get('/download/:hash', function(req, res){  	
+  	res.sendfile('images/' + req.params.hash);
+});
+
 app.get('/', function(req, res){
   	res.sendfile('templates/index.html');
 });
 
 app.post('/', function(req, res){
   	d.url(req.param('url'), function (hex) {
-  		res.send(hex);
+
+  		// res.send(hex);
+
+  		res.type('jpg');
+  		res.sendfile('images/' + hex);
   	});
 });
 
 app.listen(3000);
+
+// http://localhost:3000/images/65ae8fde9ea8daa409ae264407e6893c
